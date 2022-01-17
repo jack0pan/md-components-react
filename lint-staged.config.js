985b264cf05a2e969361a2e7a1374eec8a069e17
Jack Pan <i@jackpan.me>
2022-01-17 17:54:36 +0800

    style: replace ts-standard with eslint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   '**/*': 'prettier --write --ignore-unknown',
-  '**/*.js': 'standard --fix',
+  '**/*.{js,ts,tsx}': 'eslint --fix',
   'src/**/*.scss': 'stylelint --cache --fix --custom-syntax postcss-scss',
-  'src/**/*.{ts,tsx}': 'ts-standard --fix'
 }
