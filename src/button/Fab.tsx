688ae88b385061fc5120bd0af46c77355afc607d
Jack Pan <i@jackpan.me>
2022-01-29 17:37:21 +0800

    refactor(fab): rewrite the implementation of fab styles

--- src/button/Fab.tsx
+++ src/button/Fab.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
 import { FabProps } from './interfaces';
-import { fabClassName, fabIconClassName } from './styles';
 
 const Fab = React.forwardRef<HTMLButtonElement, FabProps>((props, ref) => {
   const { className, icon, size, style, color, ...rest } = props;
+  const classPrefix = 'material-fab';
+  const classNames = [
+    className,
+    classPrefix,
+    `${classPrefix}--${color}`,
+    `${classPrefix}--${size}`,
+  ];
   return (
-    <button
-      className={fabClassName(color, size, className)}
-      style={style}
-      role="presentation"
-      ref={ref}
-      {...rest}
-    >
-      <span className={fabIconClassName(size)}>{icon}</span>
+    <button className={classNames.join(' ')} style={style} ref={ref} {...rest}>
+      <span className={`${classPrefix}__icon material-icon`}>{icon}</span>
     </button>
   );
 });
