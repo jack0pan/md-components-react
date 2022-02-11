a10b3376d1ad73e9b054def79891fb3954b4643b
Pan Jie <panjie@growingio.com>
2022-02-11 17:17:46 +0800

    test: replace ts-jest with @swc/jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,20 @@
 /* eslint-disable tsdoc/syntax */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/index.ts', '!src/**/demos/*', '!src/**/__test__/*'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/index.ts',
+    '!src/**/interfaces.ts',
+    '!src/**/demos/*',
+    '!src/**/__test__/*',
+  ],
   coverageReporters: ['text', 'html'],
   moduleNameMapper: {
     '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
   },
-  preset: 'ts-jest',
+  // preset: 'ts-jest',
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
   testEnvironment: 'jest-environment-jsdom',
 };
