5c35307ebec26e2ec0996c9373e7373a44ac7165
Pan Jie <panjie@growingio.com>
2022-02-12 21:03:52 +0800

    build(gulp): update bundleCss task

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,35 @@
 /* eslint-disable tsdoc/syntax */
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const sass = require('gulp-sass')(require('sass'));
-const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
-const cssnano = require('gulp-cssnano');
-const through2 = require('through2');
-const ts = require('gulp-typescript');
-const rimraf = require('rimraf');
-const concat = require('gulp-concat');
-const rename = require('gulp-rename');
-const combine = require('gulp-scss-combine');
-const rollup = require('rollup');
-const rollupCommonjs = require('@rollup/plugin-commonjs');
-const rollupTS = require('@rollup/plugin-typescript');
-const { nodeResolve: rollupNodeResolve } = require('@rollup/plugin-node-resolve');
-const { terser: rollupTerser } = require('rollup-plugin-terser');
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const sourcemaps = require("gulp-sourcemaps");
+const cssnano = require("gulp-cssnano");
+const through2 = require("through2");
+const ts = require("gulp-typescript");
+const rimraf = require("rimraf");
+const rename = require("gulp-rename");
+const rollup = require("rollup");
+const rollupCommonjs = require("@rollup/plugin-commonjs");
+const rollupTS = require("@rollup/plugin-typescript");
+const { nodeResolve: rollupNodeResolve } = require("@rollup/plugin-node-resolve");
+const { terser: rollupTerser } = require("rollup-plugin-terser");
+const postcss = require("gulp-postcss");
+
+const DIST_PATH = "dist";
 
 const paths = {
   dest: {
-    lib: 'lib',
-    esm: 'es',
-    dist: 'dist',
+    lib: "lib",
+    esm: "es",
   },
-  styles: ['src/**/*.scss', '!src/**/demos/*'],
-  babelScripts: ['src/**/*.{ts,tsx}', '!src/**/{demos,__test__}/*.{ts,tsx}'],
-  tsScripts: ['src/**/*.{ts,tsx}', '!src/**/{demos,style,__test__}/*.{ts,tsx}'],
+  babelScripts: ["src/**/*.{ts,tsx}", "!src/**/{demos,__test__}/*.{ts,tsx}"],
+  tsScripts: ["src/**/*.{ts,tsx}", "!src/**/{demos,style,__test__}/*.{ts,tsx}"],
 };
 const babelConfig = {
   env: {
     esm: {
       presets: [
         [
-          '@babel/preset-env',
+          "@babel/preset-env",
           {
             modules: false,
           },
@@ -40,7 +37,7 @@ const babelConfig = {
       ],
       plugins: [
         [
-          '@babel/plugin-transform-runtime',
+          "@babel/plugin-transform-runtime",
           {
             useESModules: true,
           },
@@ -48,45 +45,57 @@ const babelConfig = {
       ],
     },
   },
-  plugins: ['@babel/plugin-transform-runtime'],
-  presets: ['@babel/preset-env', '@babel/typescript', '@babel/react'],
+  plugins: ["@babel/plugin-transform-runtime"],
+  presets: ["@babel/preset-env", "@babel/typescript", "@babel/react"],
 };
 const tsConfig = {
   esModuleInterop: true,
   skipLibCheck: true,
   declaration: true,
   emitDeclarationOnly: true,
-  jsx: 'react',
+  jsx: "react",
 };
+
+function clean(done) {
+  rimraf.sync(DIST_PATH);
+  rimraf.sync(paths.dest.lib);
+  rimraf.sync(paths.dest.esm);
+  done(0);
+}
+
+function bundleCss() {
+  return gulp
+    .src("src/index.css")
+    .pipe(postcss())
+    .pipe(gulp.dest(DIST_PATH))
+    .pipe(sourcemaps.init())
+    .pipe(cssnano({ zindex: false, reduceIdents: false }))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(DIST_PATH));
+}
 
 async function compileUMD() {
   const bundle = await rollup.rollup({
-    input: './src/index.ts',
+    input: "./src/index.ts",
     plugins: [rollupCommonjs(), rollupNodeResolve(), rollupTS()],
   });
 
   const umdOutputConfig = {
-    format: 'umd',
-    globals: { react: 'React' },
-    name: 'md-components-react',
+    format: "umd",
+    globals: { react: "React" },
+    name: "md-components-react",
     sourcemap: true,
   };
   await bundle.write({
     ...umdOutputConfig,
-    file: './dist/index.js',
+    file: `./${DIST_PATH}/index.js`,
   });
   await bundle.write({
     ...umdOutputConfig,
     plugins: [rollupTerser()],
-    file: './dist/index.min.js',
+    file: `./${DIST_PATH}/index.min.js`,
   });
-}
-
-function clean(done) {
-  rimraf.sync(paths.dest.dist);
-  rimraf.sync(paths.dest.lib);
-  rimraf.sync(paths.dest.esm);
-  done(0);
 }
 
 /**
@@ -105,7 +114,7 @@ function cssInjection(content) {
   return content
     .replace(/\/style\/?'/g, "/style/css'")
     .replace(/\/style\/?"/g, '/style/css"')
-    .replace(/\.scss/g, '.css');
+    .replace(/\.scss/g, ".css");
 }
 
 /**
@@ -127,7 +136,7 @@ function compileScripts(babelEnv, destDir) {
         if (file.path.match(/(\/|\\)style(\/|\\)index\.js/)) {
           const content = file.contents.toString(encoding);
           file.contents = Buffer.from(cssInjection(content));
-          file.path = file.path.replace(/index\.js/, 'css.js');
+          file.path = file.path.replace(/index\.js/, "css.js");
           this.push(file);
           next();
         } else {
@@ -135,7 +144,7 @@ function compileScripts(babelEnv, destDir) {
         }
       })
     )
-    .pipe(sourcemaps.write('.'))
+    .pipe(sourcemaps.write("."))
     .pipe(gulp.dest(destDir));
 }
 
@@ -146,41 +155,15 @@ function compileTypes() {
 
 function compileCJS() {
   const { dest } = paths;
-  return compileScripts('cjs', dest.lib);
+  return compileScripts("cjs", dest.lib);
 }
 
 function compileESM() {
   const { dest } = paths;
-  return compileScripts('esm', dest.esm);
-}
-
-function compileScss() {
-  return gulp
-    .src(paths.styles)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest(paths.dest.lib))
-    .pipe(gulp.dest(paths.dest.esm))
-    .pipe(concat('index.css'))
-    .pipe(gulp.dest(paths.dest.dist))
-    .pipe(sourcemaps.init())
-    .pipe(cssnano({ zindex: false, reduceIdents: false }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.dest.dist));
-}
-
-function copyScss() {
-  return gulp
-    .src(paths.styles)
-    .pipe(gulp.dest(paths.dest.lib))
-    .pipe(gulp.dest(paths.dest.esm))
-    .pipe(combine())
-    .pipe(concat('index.scss'))
-    .pipe(gulp.dest(paths.dest.dist));
+  return compileScripts("esm", dest.esm);
 }
 
 exports.default = gulp.series(
   clean,
-  gulp.parallel(compileTypes, compileScss, gulp.series(compileCJS, compileESM), compileUMD, copyScss)
+  gulp.parallel(bundleCss, gulp.series(compileCJS, compileESM), compileTypes, compileUMD)
 );
