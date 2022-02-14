3532311105b549a4b74bec9bfa3b6b7520d681e2
Pan Jie <panjie@growingio.com>
2022-02-14 22:24:48 +0800

    feat: supoort dark theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -14,3 +14,34 @@ export const parameters = {
     theme,
   },
 };
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Theme preferences',
+    defaultValue: 'auto',
+    toolbar: {
+      icon: 'switchalt',
+      items: [
+        { value: 'auto', right: '⚙️', title: 'Sync with system' },
+        { value: 'light', right: '🌞', title: 'Light theme' },
+        { value: 'dark', right: '🌛', title: 'Dark theme' },
+      ],
+    },
+  },
+};
+
+const withWrapper = (Story, context) => {
+  const {
+    globals: { theme },
+  } = context;
+  document.documentElement.setAttribute('data-color-mode', theme);
+
+  return (
+    <div style={{ padding: '1rem' }}>
+      <Story />
+    </div>
+  );
+};
+
+export const decorators = [withWrapper];
